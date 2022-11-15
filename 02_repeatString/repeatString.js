8ca5c47bb4f266cfcae48441d4d36a5992a5bc4e
James Martin <117032706+Jamtin68@users.noreply.github.com>
2022-11-15 19:56:42 +0000

    completed 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  let a = string;
+  if (num === 0) {
+    string = "";
+  } else if (num < 0) {
+    string = "ERROR";
+  } else {
+      for (i = 1; i < num; i++) {
+      string = string + a;
+  }
+}  
+  return string;
+}
 
-};
 
 // Do not edit below this line
 module.exports = repeatString;
