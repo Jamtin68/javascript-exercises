fbe1e7d135a8fe49ac2de8ae8f8740cda4e33366
James Martin <117032706+Jamtin68@users.noreply.github.com>
2022-11-16 13:23:38 +0000

    exercise 3 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  let b = "";
+  for (i = 1; i < string.length+1; i++) {
+    b = b + string.charAt(string.length-i); 
 
-};
-
+  }
+  return b;
+}
 // Do not edit below this line
 module.exports = reverseString;
