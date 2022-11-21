cba12fde90bef7bf1876ce0fad9f6590c21c9081
James Martin <117032706+Jamtin68@users.noreply.github.com>
2022-11-21 15:02:48 +0000

    test 7 completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(Fi) {
+  Co = (Fi - 32) * 5 / 9;
+  Co = Math.round(Co * 10) / 10;
+  return Co;
 };
 
-const ctof = function() {
-
+const ctof = function(Ci) {
+  Fo = Ci * 9 / 5 + 32;
+  Fo = Math.round(Fo * 10) / 10; 
+  return Fo;
 };
 
 // Do not edit below this line
