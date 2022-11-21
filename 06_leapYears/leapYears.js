ef78d3d0572a9275ba9e3006d1966bbffa6b3cab
James Martin <117032706+Jamtin68@users.noreply.github.com>
2022-11-21 14:46:36 +0000

    test 6 completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,22 @@
-const leapYears = function() {
-
-};
+const leapYears = function(year) {
+    let lYear;
+    if (year % 400 === 0) {
+      lYear = true;
+    } else {
+      if (year % 100 === 0) {
+        lYear = false;
+      } else {
+        if (year % 4 === 0) {
+          lYear = true;
+        } else {
+          lYear = false;
+        }
+        return lYear;
+      }
+      return lYear;
+    }
+    return lYear;
+  };
 
 // Do not edit below this line
 module.exports = leapYears;
