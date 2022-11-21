6d55aa795a0e792f960aea0ff69c1a0be1c49d4c
James Martin <117032706+Jamtin68@users.noreply.github.com>
2022-11-21 13:58:46 +0000

    test 5 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(n1, n2) {
+    
+  console.log(typeof(n1), n2, n1 + n2);
+  if ((n1 < 0) || (n2 < 0) || (typeof(n1) !== 'number') || (typeof(n2) !== 'number')) {
+      sum = 'ERROR';
+  } else {
+      let sum = 0;
+      if (n1 > n2) {
+        for (i = n2; i <= n1; i++) {
+          sum = sum + i;
+        }
+      } else if (n1 < n2) {
+        for (i = n1; i <= n2; i++) {
+          sum = sum + i;      
+        }
+      }
+      return sum;
+  } return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
