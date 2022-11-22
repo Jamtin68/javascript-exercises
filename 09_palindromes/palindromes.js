89cfd20bb7d7d3368bf9bbd8f51c54f52cfec4da
James Martin <117032706+Jamtin68@users.noreply.github.com>
2022-11-22 16:32:59 +0000

    test 9 complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,27 @@
-const palindromes = function () {
-
-};
+const palindromes = function (word) {
+    for (i = 0; i < word.length; i++) {
+      let word2 = word
+      if (Boolean((word.charAt(i)).match(/^[A-Za-z]*$/)) == false) {      
+        word = word.substring(0, i) + word.substring(i + 1, word.length);
+        i--;      
+      }    
+    }
+    word = word.toLowerCase();
+    let d = 0;
+    for (i = 0; i < word.length; i++) {
+      if (word.charAt(i) !== word.charAt(word.length - i - 1)) {
+        d++;
+      }
+    }
+    if (d > 0) {
+      let result = false; 
+      return result;   
+    } else {
+      let result = true
+      return result;
+    }
+    
+  };
 
 // Do not edit below this line
 module.exports = palindromes;
