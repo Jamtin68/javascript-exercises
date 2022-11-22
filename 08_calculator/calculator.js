d6e0ee07cecaff1f57d751ae2d8fa0a285f44c18
James Martin <117032706+Jamtin68@users.noreply.github.com>
2022-11-22 14:51:53 +0000

    test 8 complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,59 @@
-const add = function() {
-	
+const add = function(add1, add2) {
+	return add1 + add2; 
 };
 
-const subtract = function() {
-	
+const subtract = function(subtract1 , subtract2) {
+	return subtract1 - subtract2;
 };
 
-const sum = function() {
-	
+const sum = function(sArray) {
+  let summingUp = 0;
+	for (i = 0; i < sArray.length; i++) {
+    summingUp = summingUp + sArray[i];
+  }
+  return summingUp;
 };
 
-const multiply = function() {
+const multiply = function(sMultiply) {
 
+  if (sMultiply.length === 0) {
+    product = 0;
+  } else {
+    product = 1;
+    for (i = 0; i < sMultiply.length; i++) {
+      product = product * sMultiply[i];
+    }
+    return product;
+  }
+  return product;
 };
 
-const power = function() {
+const power = function(b, p) {
 	
+  if (p < 0) {
+    let bc = 'ERROR';
+    return bc;
+  } else if (p === 0) {
+    let bc = 1;
+    return bc;
+  } else {
+    let bc = 1;
+    for (i = 1; i < p + 1; i++) {
+      bc = bc * b;
+    }
+    return bc;
+  }
+  return bc;
 };
 
-const factorial = function() {
-	
+const factorial = function(input) {	
+  let f = 1;
+  if (input > 0) {
+    for (i = 1; i < input + 1; i++) {
+      f = f * i
+    }
+  }
+  return f;
 };
 
 // Do not edit below this line
