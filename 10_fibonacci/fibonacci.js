11b94022cbcb2a788360cb18375dac97a5cf0062
James Martin <117032706+Jamtin68@users.noreply.github.com>
2022-11-22 17:08:57 +0000

    test 9 complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (typeof(num) === 'string') {
+      num = Number(num);
+    } 
+    
+    let sum1 = 1;
+    let sum2 = 1
+    for (i = 1; i <= num - 2; i++) {
+      sum3 = sum1 + sum2;
+      sum1 = sum2;
+      sum2 = sum3;
+    }
+    if ((num === 1) || (num === 2)){
+      sum3 = 1;
+    } else if (num <= 0) {
+        let sum3 = 'OOPS';
+        return sum3;
+    }
 
-};
+    return sum3;
+    
+  };
+  
 
 // Do not edit below this line
 module.exports = fibonacci;
