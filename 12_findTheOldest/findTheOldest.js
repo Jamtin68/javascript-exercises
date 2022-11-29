2f135a66538e9d575e9d66239918011e07b02fa6
James Martin <117032706+Jamtin68@users.noreply.github.com>
2022-11-29 14:29:38 +0000

    test 12 complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    ages = [];
+    for (i = 0; i < people.length; i++) {
+        if (people[i].yearOfDeath == null) (
+            people[i].yearOfDeath = new Date().getFullYear()
+        )
+        ages.push(people[i].yearOfDeath - people[i].yearOfBirth);        
+    } 
+    let oldest = 0;  
+    for (i = 1; i < ages.length; i++) {
+      if (ages[i] > ages[i - 1]) {
+        oldest = i;
+      }
+    }
+    people = people[oldest];
 
-};
+    return people;
+  };
 
 // Do not edit below this line
 module.exports = findTheOldest;
